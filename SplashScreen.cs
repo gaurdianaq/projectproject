73b9662b2f1c741d05f8aec4c53d9e84d8fea232
Evan <gaurdianaq@protonmail.com>
2024-09-30 20:08:32 -0400

    now has listens for a random continue button input

--- SplashScreen.cs
+++ SplashScreen.cs
@@ -2,48 +2,72 @@ using Godot;
 using System;
 using System.Collections.Generic;
 
-//TODO Load Splash Screen Config
+//Do we want to have the splash screen config in a separate config file that is loaded at runtime? 
 //TODO Ensure text and background image scale programmatically
 //TODO Set background image programmatically
 public partial class SplashScreen : Control
 {
-	private class ScreenData
-	{	
-		public string Title { get; set; }
-		public string Subtitle { get; set; }
+    private class ScreenData
+    {
+        public string Title { get; set; }
+        public string Subtitle { get; set; }
 
-		public ScreenData(string title, string subtitle) {
-			Title = title;
-			Subtitle = subtitle;
-		}
+        public ScreenData(string title, string subtitle)
+        {
+            Title = title;
+            Subtitle = subtitle;
+        }
 
-	}
-	private Label title;
-	private Label subTitle;
+    }
+    private Label title;
+    private Label subTitle;
+    private Label continueLbl;
 
-	private List<ScreenData> possibleTitle = new List<ScreenData>();
+    private Key continueKey;
+    private string keyString;
+
+    private List<ScreenData> possibleTitle = new List<ScreenData>();
 
     public override void _EnterTree()
     {
-        base._EnterTree();
-		possibleTitle.Add(new ScreenData("Booty Slayer", "A Pirates Hunt for Mimics"));
-		possibleTitle.Add(new ScreenData("Living Chocolate Hunter", "Quest for Deliciousness"));
-		possibleTitle.Add(new ScreenData("Trogdors Day Out", "Burnination Has Arrived!"));
-
+        possibleTitle.Add(new ScreenData("Booty Slayer", "A Pirates Hunt for Mimics"));
+        possibleTitle.Add(new ScreenData("Living Chocolate Hunter", "Quest for Deliciousness"));
+        possibleTitle.Add(new ScreenData("Trogdors Day Out", "Burnination Has Arrived!"));
     }
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
-	{
-		Random rnd = new Random();
-		int titleNum = rnd.Next(0, possibleTitle.Count);
-		title = GetNode<Label>("Title");
-		subTitle = title.GetNode<Label>("Subtitle");
-		title.Text = possibleTitle[titleNum].Title;
-		subTitle.Text = possibleTitle[titleNum].Subtitle;
-	}
+    {
+        Random rnd = new Random();
+        keyString = rnd.Next(33, 91).ToString();
+        continueKey = (Key)Enum.Parse(typeof(Key), keyString);
+        int titleNum = rnd.Next(0, possibleTitle.Count);
+        title = GetNode<Label>("Title");
+        subTitle = title.GetNode<Label>("Subtitle");
+        continueLbl = title.GetNode<Label>("Continue");
+        title.Text = possibleTitle[titleNum].Title;
+        subTitle.Text = possibleTitle[titleNum].Subtitle;
+        continueLbl.Text = "Press " + (char)Int32.Parse(keyString) + " to continue, any other key will result in termination";
+    }
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
-	}
+    // Called every frame. 'delta' is the elapsed time since the previous frame.
+    public override void _Process(double delta)
+    {
+    }
+    //TODO figure out how to handle combo keys for things like # or &, or just include a silly message telling the user they have poor luck and to restart the app
+    public override void _UnhandledInput(InputEvent @event)
+    {
+        if (@event is InputEventKey eventKey)
+        {
+            if (eventKey.Pressed && eventKey.Keycode == continueKey)
+            {
+                GD.Print("you chose wisely");
+                //TODO add scene transition logic
+            }
+            else if (eventKey.IsReleased() && eventKey.Keycode != continueKey && eventKey.Keycode != Key.Shift)
+            {
+                GD.Print("You have chosen termination");
+                GetTree().Quit();
+            }
+        }
+    }
 }
