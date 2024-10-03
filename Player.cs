82f1dee7315b5cdf471c6f7148c6378a71e3ef59
Dylan Renwick <skidsirongenesisdev@gmail.com>
2024-10-03 14:12:06 -0400

    Added movement to Player

---
+++ Player.cs
@@ -0,0 +1,15 @@
+using Godot;
+
+public partial class Player : CharacterBody2D
+{
+	[Export]
+	public float Speed { get; set; } = 300.0f;
+
+	public override void _PhysicsProcess(double delta)
+	{
+		Vector2 inputVec = Input.GetVector("left", "right", "up", "down");
+		Velocity = inputVec * Speed;
+		
+		MoveAndSlide();
+	}
+}
